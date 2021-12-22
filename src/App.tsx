65cd6071f79a9522f6d6f1bfa932083844675a76
NickSettler <moiseevnikita14@gmail.com>
2021-12-22 20:17:36 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">{/*<Main />*/}</Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
