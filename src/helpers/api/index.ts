a210cebc055b24d0368ff8d70264e7ed7d2d4f61
NickSettler <moiseevnikita14@gmail.com>
2021-12-25 00:51:44 +0100

    adds api class

---
+++ src/helpers/api/index.ts
@@ -0,0 +1,22 @@
+import { Directus, DirectusOptions } from "@directus/sdk";
+
+export default class Api extends Directus<any> {
+  private static _instance: Api;
+  private static _baseUrl: string = process.env.REACT_APP_API_URL!;
+
+  private constructor(url: string, options: DirectusOptions) {
+    super(url, options);
+  }
+
+  public static getInstance(): Api {
+    if (!Api._instance) {
+      Api._instance = new Api(Api._baseUrl, {
+        auth: {
+          autoRefresh: true,
+          // mode: "json",
+        },
+      });
+    }
+    return Api._instance;
+  }
+}
