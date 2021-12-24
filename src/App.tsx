d4b6762448eafe7495854456b673e697d415d0d6
NickSettler <moiseevnikita14@gmail.com>
2021-12-24 21:18:45 +0100

    converts App to arrow function and adds Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/">{/*<Main />*/}</Route>
-      </Routes>
-    </Router>
-  );
-}
+const App = (): JSX.Element => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/">{/*<Main />*/}</Route>
+    </Routes>
+  </Router>
+);
 
 export default App;
