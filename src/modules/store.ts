cb31db4cffa19e7c2130b8d20ebc6f98a3b46425
NickSettler <moiseevnikita14@gmail.com>
2021-12-24 23:43:18 +0100

    adds redux dev tools middleware

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,9 +1,18 @@
-import { combineReducers, createStore } from "@reduxjs/toolkit";
+import { combineReducers, compose, createStore } from "@reduxjs/toolkit";
 import authReducer, { moduleName as authModuleName } from "./auth";
+
+let middleware: Array<Function> = [];
+
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
 
 const rootReducer = combineReducers({
   [authModuleName]: authReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, compose(...middleware));
 
 export default store;
