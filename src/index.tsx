420942c687b335bda457e6e3c5b6d494989b5205
NickSettler <moiseevnikita14@gmail.com>
2021-12-24 23:56:42 +0100

    adds sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,14 @@ import { Provider } from "react-redux";
 import store from "./modules/store";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./helpers/theme/theme";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://ebd88944e5894badb4f7f44760427571@o525460.ingest.sentry.io/6123029",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
