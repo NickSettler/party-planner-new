063c05c9c67c7615b3d2006bf5ab9c3b1ce6fa46
NickSettler <moiseevnikita14@gmail.com>
2021-12-24 21:18:03 +0100

    adds default store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./modules/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/modules/store.ts
@@ -0,0 +1,6 @@
+import { combineReducers, createStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers([]);
+const store = createStore(rootReducer);
+
+export default store;
