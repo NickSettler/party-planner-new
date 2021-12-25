034e8561407036b69422dede49115353308c28ca
NickSettler <moiseevnikita14@gmail.com>
2021-12-25 02:18:51 +0100

    adds main saga

--- src/modules/saga.ts
+++ src/modules/saga.ts
@@ -1,3 +1,9 @@
-function* rootSaga() {}
+import { all, fork } from "redux-saga/effects";
+import { authSaga } from "./auth";
+import { userSaga } from "./user";
+
+function* rootSaga() {
+  yield all([fork(authSaga), fork(userSaga)]);
+}
 
 export default rootSaga;
