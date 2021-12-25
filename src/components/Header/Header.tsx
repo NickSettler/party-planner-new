b60e2f9e6f0d2bcc1ddf8ea393912fb42aadd35a
NickSettler <moiseevnikita14@gmail.com>
2021-12-25 14:07:05 +0100

    adds conditional rendering header buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,42 @@
 import { connect } from "react-redux";
-import {
-  HeaderAppBar,
-  HeaderTitle,
-  HeaderToolbar,
-} from "./styled-components/Header";
+import { HeaderAppBar, HeaderTitle, HeaderToolbar } from "./styled/Header";
 import LinkButton from "../../uikit/LinkButton/LinkButton";
+import { userLoggedSelector } from "../../modules/user";
+import { Dispatch } from "@reduxjs/toolkit";
+import { HeaderPropsT } from "./types/Header.types";
 
-const Header = (): JSX.Element => {
+const Header = (props: HeaderPropsT): JSX.Element => {
+  const { userLogged } = props;
+
   return (
     <HeaderAppBar position={"static"}>
       <HeaderToolbar>
         <HeaderTitle to={"/"} variant={"h6"} color={"inherit"}>
           Party Manager
         </HeaderTitle>
-        <LinkButton to={"/signin"} variant={"text"} color={"inherit"}>
-          Sign In
-        </LinkButton>
-        <LinkButton to={"/signup"} variant={"text"} color={"inherit"}>
-          Sign Up
-        </LinkButton>
+        {userLogged ? (
+          <LinkButton to={"/logout"} color={"inherit"}>
+            Logout
+          </LinkButton>
+        ) : (
+          <>
+            <LinkButton to={"/signin"} variant={"text"} color={"inherit"}>
+              Sign In
+            </LinkButton>
+            <LinkButton to={"/signup"} variant={"text"} color={"inherit"}>
+              Sign Up
+            </LinkButton>
+          </>
+        )}
       </HeaderToolbar>
     </HeaderAppBar>
   );
 };
 
-export default connect(null, null)(Header);
+const mapStateToProps = (state: any) => ({
+  userLogged: userLoggedSelector(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ src/components/Header/types/Header.types.ts
@@ -0,0 +1,3 @@
+export type HeaderPropsT = {
+  userLogged: boolean;
+};
