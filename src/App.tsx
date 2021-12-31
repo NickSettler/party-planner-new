68909f83b181fc8abeb39e3c0d6bc51685de7466
NickSettler <moiseevnikita14@gmail.com>
2021-12-31 17:41:21 +0100

    adds protected dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import DashboardIndex from "./pages/dashboard/Index/DashboardIndex";
+import ProtectedRoute from "./uikit/ProtectedRoute/ProtectedRoute";
 
 const App = (): JSX.Element => (
   <Router>
@@ -12,6 +14,14 @@ const App = (): JSX.Element => (
       <Route path="/" element={<MainPage />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route
+        path={"/dashboard/*"}
+        element={
+          <ProtectedRoute>
+            <DashboardIndex />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </Router>
 );
