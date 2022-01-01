0848e37e481661d0b34cbf80ec7af6589487a16b
NickSettler <moiseevnikita14@gmail.com>
2022-01-01 23:35:51 +0100

    adds reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Router from "./routes";
+import GlobalStyles from "./helpers/theme/GlobalStyles";
 
 const App = (): JSX.Element => (
   <BrowserRouter>
     <Header />
+    <GlobalStyles />
     <Router />
   </BrowserRouter>
 );
