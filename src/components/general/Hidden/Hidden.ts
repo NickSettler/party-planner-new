0df4912acec096a12be97fe369162d7c86e394ae
NickSettler <moiseevnikita14@gmail.com>
2022-01-02 04:09:22 +0100

    adds hidden component

---
+++ src/components/general/Hidden/Hidden.ts
@@ -0,0 +1,20 @@
+import { HiddenPropsT } from "./hidden.types";
+import { Breakpoint, Theme, useMediaQuery } from "@mui/material";
+
+const Hidden = ({ width, children }: HiddenPropsT): JSX.Element | null => {
+  const breakpoint = width.substring(0, 2) as Breakpoint;
+
+  const hiddenUp = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up(breakpoint)
+  );
+  const hiddenDown = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down(breakpoint)
+  );
+
+  if (width.includes("Down")) return hiddenDown ? null : children;
+  if (width.includes("Up")) return hiddenUp ? null : children;
+
+  return null;
+};
+
+export default Hidden;
---
+++ src/components/general/Hidden/hidden.types.ts
@@ -0,0 +1,4 @@
+export type HiddenPropsT = {
+  width: string;
+  children: JSX.Element;
+};
