9182ffe44fe9df39920987c9b0e4ce1813bd20e4
NickSettler <moiseevnikita14@gmail.com>
2022-01-04 21:48:47 +0100

    changes sign up page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -2,16 +2,12 @@ import { connect } from "react-redux";
 import useSignUpPage from "./useSignUpPage";
 import { SignUpPagePropsT } from "./types/SignUpPage.types";
 import { userLoggedSelector } from "../../modules/user";
-import { Navigate } from "react-router-dom";
+import { Link as RouterLink, Navigate } from "react-router-dom";
 import {
-  SignInLink,
-  SignUpActions,
   SignUpBox,
-  SignUpForm,
+  SignUpContentSection,
+  SignUpIllustrationCard,
 } from "./styled/SignUpPage";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import React from "react";
@@ -22,6 +18,13 @@ import {
   signUpRequestFailedSelector,
   signUpRequestLoadingSelector,
 } from "../../modules/auth";
+import Hidden from "../../uikit/Hidden";
+import Box from "@mui/material/Box";
+import UnlockImage from "../../assets/illustrations/unlock.svg";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import AuthLayout from "../../layouts/AuthLayout";
+import { Link } from "@mui/material";
 
 const SignUpPage = (props: SignUpPagePropsT): JSX.Element => {
   const {
@@ -48,95 +51,118 @@ const SignUpPage = (props: SignUpPagePropsT): JSX.Element => {
 
   return (
     <>
+      <AuthLayout>
+        <>
+          Already have an account? &nbsp;{" "}
+          <Link
+            to={"/signin"}
+            component={RouterLink}
+            underline={"none"}
+            color={"secondary"}
+            variant={"subtitle2"}
+          >
+            Login
+          </Link>
+        </>
+      </AuthLayout>
       <SignUpBox>
-        <SignUpForm onSubmit={handleSubmit}>
-          <Card>
-            <CardHeader title={"Sign Up"} />
-            <CardContent>
-              <TextField
-                type={"email"}
-                name={"email"}
-                value={email}
-                onBlur={() => validateField("email")}
-                onChange={handleChange}
-                label={"Email"}
-                fullWidth
-                margin={"dense"}
-                error={!!errors.email}
-                helperText={errors.email}
-                autoComplete={"email"}
-                disabled={signUpRequestLoading}
-              />
-              <TextField
-                type={"text"}
-                name={"username"}
-                value={username}
-                onBlur={() => validateField("username")}
-                onChange={handleChange}
-                label={"Username"}
-                fullWidth
-                margin={"dense"}
-                error={!!errors.username}
-                helperText={errors.username}
-                autoComplete={"username"}
-                disabled={signUpRequestLoading}
-              />
-              <TextField
-                type={"password"}
-                name={"password"}
-                value={password}
-                onBlur={() => validateField("password")}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  handleChange(e);
-                  validateField("password");
-                  validateField("confirmPassword");
-                }}
-                label={"Password"}
-                fullWidth
-                margin={"dense"}
-                error={!!errors.password}
-                helperText={errors.password}
-                autoComplete={"new-password"}
-                disabled={signUpRequestLoading}
-              />
-              <TextField
-                type={"password"}
-                name={"confirmPassword"}
-                value={confirmPassword}
-                onBlur={() => validateField("confirmPassword")}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  handleChange(e);
-                  validateField("password");
-                  validateField("confirmPassword");
-                }}
-                label={"Password Confirmation"}
-                fullWidth
-                margin={"dense"}
-                error={!!errors.confirmPassword}
-                helperText={errors.confirmPassword}
-                autoComplete={"new-password"}
-                disabled={signUpRequestLoading}
-              />
-              {signUpRequestFailed && (
-                <Typography color={"error"}>{signUpRequestError}</Typography>
-              )}
-            </CardContent>
-            <SignUpActions>
-              <Button
-                type={"submit"}
-                color={"secondary"}
-                variant={"text"}
-                disabled={signUpRequestLoading}
-              >
+        <Hidden width={"mdDown"}>
+          <SignUpIllustrationCard>
+            <Box component={"img"} src={UnlockImage} />
+          </SignUpIllustrationCard>
+        </Hidden>
+        <Container maxWidth={"sm"}>
+          <SignUpContentSection>
+            <Stack sx={{ mb: 5 }}>
+              <Typography variant={"h4"} gutterBottom>
                 Sign Up
-              </Button>
-            </SignUpActions>
-          </Card>
-        </SignUpForm>
-        <Typography>
-          If you already have an account, please{" "}
-          <SignInLink to={"/signin"}>sign in</SignInLink>
-        </Typography>
+              </Typography>
+              <Typography variant={"body1"}>
+                Enter your details below.
+              </Typography>
+            </Stack>
+            <form onSubmit={handleSubmit}>
+              <Stack>
+                <TextField
+                  type={"email"}
+                  name={"email"}
+                  value={email}
+                  onBlur={() => validateField("email")}
+                  onChange={handleChange}
+                  label={"Email"}
+                  fullWidth
+                  margin={"dense"}
+                  error={!!errors.email}
+                  helperText={errors.email}
+                  autoComplete={"email"}
+                  disabled={signUpRequestLoading}
+                />
+                <TextField
+                  type={"text"}
+                  name={"username"}
+                  value={username}
+                  onBlur={() => validateField("username")}
+                  onChange={handleChange}
+                  label={"Username"}
+                  fullWidth
+                  margin={"dense"}
+                  error={!!errors.username}
+                  helperText={errors.username}
+                  autoComplete={"username"}
+                  disabled={signUpRequestLoading}
+                />
+                <TextField
+                  type={"password"}
+                  name={"password"}
+                  value={password}
+                  onBlur={() => validateField("password")}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    handleChange(e);
+                    validateField("password");
+                    validateField("confirmPassword");
+                  }}
+                  label={"Password"}
+                  fullWidth
+                  margin={"dense"}
+                  error={!!errors.password}
+                  helperText={errors.password}
+                  autoComplete={"new-password"}
+                  disabled={signUpRequestLoading}
+                />
+                <TextField
+                  type={"password"}
+                  name={"confirmPassword"}
+                  value={confirmPassword}
+                  onBlur={() => validateField("confirmPassword")}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    handleChange(e);
+                    validateField("password");
+                    validateField("confirmPassword");
+                  }}
+                  label={"Password Confirmation"}
+                  fullWidth
+                  margin={"dense"}
+                  error={!!errors.confirmPassword}
+                  helperText={errors.confirmPassword}
+                  autoComplete={"new-password"}
+                  disabled={signUpRequestLoading}
+                />
+                {signUpRequestFailed && (
+                  <Typography color={"error"}>{signUpRequestError}</Typography>
+                )}
+                <Button
+                  type={"submit"}
+                  color={"secondary"}
+                  variant={"contained"}
+                  disabled={signUpRequestLoading}
+                  sx={{ mt: 0.5 }}
+                >
+                  Sign Up
+                </Button>
+              </Stack>
+            </form>
+          </SignUpContentSection>
+        </Container>
       </SignUpBox>
     </>
   );
