1eda38296facdf454ca5b16ac98ae55efad4943b
NickSettler <moiseevnikita14@gmail.com>
2022-01-04 21:47:14 +0100

    changes auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,40 @@
-import Container from "@mui/material/Container";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import Hidden from "../uikit/Hidden";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
 
-const AuthLayout = (): JSX.Element => {
+const HeaderElement = styled("header")(({ theme }) => ({
+  top: 0,
+  zIndex: 9,
+  lineHeight: 0,
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
+  padding: theme.spacing(3),
+  justifyContent: "flex-end",
+
+  [theme.breakpoints.up("md")]: {
+    alignItems: "flex-start",
+    padding: theme.spacing(7, 5, 0, 7),
+  },
+}));
+
+const AuthLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
   return (
-    <Container>
+    <>
       <Outlet />
-    </Container>
+      <HeaderElement>
+        <Hidden width={"smDown"}>
+          <Typography variant={"body2"}>{children}</Typography>
+        </Hidden>
+      </HeaderElement>
+    </>
   );
 };
 
