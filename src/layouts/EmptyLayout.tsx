b270cef28c520221677fa9a180fcef74e8e9a897
NickSettler <moiseevnikita14@gmail.com>
2022-01-04 21:06:35 +0100

    refactors

--- src/layouts/EmptyLayout.tsx
+++ src/layouts/EmptyLayout.tsx
@@ -9,6 +9,7 @@ const StyledHeader = styled("header")(({ theme }) => ({
   width: "100%",
   position: "absolute",
   padding: theme.spacing(3, 3, 0),
+
   [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(5, 5, 0),
   },
