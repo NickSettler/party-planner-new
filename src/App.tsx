02e2b346f4597dd527dccf85f84ac6bbd265c298
NickSettler <moiseevnikita14@gmail.com>
2022-01-04 21:06:23 +0100

    adds google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Router from "./routes";
 import GlobalStyles from "./helpers/theme/GlobalStyles";
+import "./assets/css/App.css";
 
 const App = (): JSX.Element => (
   <BrowserRouter>
