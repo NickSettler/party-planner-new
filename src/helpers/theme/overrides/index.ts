dd71cd62329877c7ce22c98d2344de24c512cea9
NickSettler <moiseevnikita14@gmail.com>
2022-01-09 16:23:08 +0100

    adds icon button override

--- src/helpers/theme/overrides/index.ts
+++ src/helpers/theme/overrides/index.ts
@@ -2,9 +2,10 @@ import { merge } from "lodash";
 import Card from "./Card";
 import { Components, Theme } from "@mui/material";
 import Button from "./Button";
+import IconButton from "./IconButton";
 
 const componentOverrides = (theme: Theme): Components => {
-  return merge(Card(theme), Button(theme));
+  return merge(Card(theme), Button(theme), IconButton(theme));
 };
 
 export default componentOverrides;
